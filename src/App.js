a70117d609dbc711241ecd5b98d81413bcbf7c2b
Jace <jacealan1@gmail.com>
2022-11-26 20:18:31 +0900

    change % to e

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import { useState, useRef, useEffect } from "react"
 import { DeleteForever, SmartButton, ArrowBack } from "@styled-icons/material"
 import "./App.css"
 
+const numComma = (num) => {
+  const str = typeof num === "number" ? num.toString.split(".") : num.split(".")
+  if (str.length > 2) {
+    alert("wrong input")
+    return `${num} `
+  } else if (str.length === 1) {
+    return `${str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")} `
+  } else {
+    return `${str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.${str[1]} `
+  }
+}
+
 function App() {
   const logBoxRef = useRef(null)
   const typedBoxRef = useRef(null)
@@ -10,6 +22,7 @@ function App() {
   const [log, setLog] = useState([])
   const [typed, setTyped] = useState("")
   const [ans, setAns] = useState("")
+  const clicked = useRef(false)
 
   const calc = () => {
     if (typed !== "") {
@@ -44,25 +57,31 @@ function App() {
       target: { name },
     } = event
 
-    if (name === "=") {
-      calc()
-    } else if (name === "clear") {
-      setTyped("")
-    } else if (name === "back") {
-      setTyped((prev) => prev.slice(0, -1))
-    } else if (name === "del") {
-      setLog([])
-    } else if (name === "erase") {
-      setQuickNum((prev) => [])
-    } else if (name === "write") {
-      ans2btn()
-    } else if (name === "ans") {
-      setTyped((prev) => prev + ans)
-    } else if (name === "dot") {
-      setTyped((prev) => prev + ".")
-    } else if (name !== undefined) {
-      setTyped((prev) => prev + name)
+    if (!clicked.current) {
+      if (name === "=") {
+        calc()
+      } else if (name === "clear") {
+        setTyped("")
+      } else if (name === "back") {
+        setTyped((prev) => prev.slice(0, -1))
+      } else if (name === "del") {
+        setLog([])
+      } else if (name === "remove") {
+        setQuickNum((prev) => [])
+      } else if (name === "write") {
+        ans2btn()
+      } else if (name === "ans") {
+        setTyped((prev) => prev + ans)
+      } else if (name === "dot") {
+        setTyped((prev) => prev + ".")
+      } else if (name !== undefined) {
+        setTyped((prev) => prev + name)
+      }
     }
+    clicked.current = true
+    setTimeout(() => {
+      clicked.current = false
+    }, 100)
   }
 
   useEffect(() => {
@@ -81,8 +100,8 @@ function App() {
     } else if (key === "Backspace") {
       setTyped((prev) => prev.slice(0, -1))
     } else if (key === "Delete" || key === "D" || key === "d") {
-      setLog("")
-    } else if (key === "E" || key === "e") {
+      setLog([])
+    } else if (key === "R" || key === "r") {
       setQuickNum([])
     } else if (key === "W" || key === "w") {
       ans2btn()
@@ -98,7 +117,9 @@ function App() {
       key === "/" ||
       key === "%" ||
       key === "(" ||
-      key === ")"
+      key === ")" ||
+      key === "E" ||
+      key === "e"
     ) {
       setTyped((prev) => prev + key)
     }
@@ -106,12 +127,12 @@ function App() {
 
   return (
     <div className="App" onKeyDown={onKeyDown}>
-      {/* <div className="title">Quick Number</div> */}
+      {/* QUICK NUMBER  */}
       <div className="quick_numbers">
         {quickNum.map((num, index) => (
           <div className="quick_number" key={index}>
             <div
-              onClick={() => setTyped((prev) => prev + quickNum[index])}
+              onClick={() => setTyped((prev) => `${prev}(${quickNum[index]})`)}
               style={{
                 width: "100px",
                 textAlign: "left",
@@ -140,40 +161,49 @@ function App() {
           </div>
         ))}
       </div>
-      {/* <div className="title">Log of Calc</div> */}
-      <div className="log" ref={logBoxRef}>
-        {log.map((line, index) => (
-          <div key={index}>
-            {line}
-            {index === log.length - 1 ? (
-              <SmartButton
-                size="24"
-                color="#285430"
-                onClick={ans2btn}
-                style={{ cursor: "pointer" }}
-              />
-            ) : null}
-          </div>
-        ))}
+      {/* LOG */}
+      <div className="log-box">
+        <div className="log" ref={logBoxRef}>
+          {log.map((line, index) => (
+            <div key={index}>
+              {line[0] === "=" ? "= " + numComma(line.slice(1)) : line}
+              {index === log.length - 1 ? (
+                <SmartButton
+                  size="24"
+                  color="#285430"
+                  onClick={ans2btn}
+                  style={{ cursor: "pointer" }}
+                />
+              ) : null}
+            </div>
+          ))}
+        </div>
         <div className="log-title">Log of Calc</div>
       </div>
+      {/* TYPED */}
       <div className="typed" ref={typedBoxRef}>
         {typed}
         <span style={{ animation: "blink-effect 1s step-end infinite" }}>
           _
         </span>
       </div>
-      <div className="buttons">
+      {/* KEYPAD */}
+      <div className="keypads">
         <button
           className="keypad"
           name="clear"
           onClick={onClick}
           style={{ backgroundColor: "#393E46", color: "white" }}
         >
-          Clear<div style={{ fontSize: "8px" }}>[esc]</div>
-        </button>
-        <button className="keypad" name="back" onClick={onClick}>
-          <ArrowBack size="24" onClick={onClick} />
+          Clear
+          <button
+            className="keypad-info"
+            name="clear"
+            onClick={onClick}
+            style={{ backgroundColor: "#393E46", color: "white" }}
+          >
+            [esc]
+          </button>
         </button>
         <button className="keypad" name="(" onClick={onClick}>
           (
@@ -181,8 +211,19 @@ function App() {
         <button className="keypad" name=")" onClick={onClick}>
           )
         </button>
-        <button className="keypad" name="%" onClick={onClick}>
-          %<div style={{ fontSize: "8px" }}>나머지</div>
+        <button className="keypad" name="back" onClick={onClick}>
+          🡠
+        </button>
+        <button className="keypad" name="e" onClick={onClick}>
+          e
+          <button
+            className="keypad-info"
+            name="e"
+            onClick={onClick}
+            style={{ backgroundColor: "#ddd" }}
+          >
+            1.2e+3
+          </button>
         </button>
         <button
           className="keypad"
@@ -190,7 +231,15 @@ function App() {
           onClick={onClick}
           style={{ backgroundColor: "#7D5A5A", color: "white" }}
         >
-          Del<div style={{ fontSize: "8px" }}>Log</div>
+          Del
+          <button
+            className="keypad-info"
+            name="del"
+            onClick={onClick}
+            style={{ backgroundColor: "#7D5A5A", color: "white" }}
+          >
+            Log
+          </button>
         </button>
         <button className="keypad" name="7" onClick={onClick}>
           7
@@ -206,11 +255,19 @@ function App() {
         </button>
         <button
           className="keypad"
-          name="erase"
+          name="remove"
           onClick={onClick}
           style={{ backgroundColor: "#0F4C75", color: "white" }}
         >
-          Erase<div style={{ fontSize: "8px" }}>QuickNum</div>
+          Remove
+          <button
+            className="keypad-info"
+            name="remove"
+            onClick={onClick}
+            style={{ backgroundColor: "#0F4C75", color: "white" }}
+          >
+            QuickNum
+          </button>
         </button>
         <button className="keypad" name="4" onClick={onClick}>
           4
@@ -231,13 +288,20 @@ function App() {
           style={{
             backgroundColor: "#3282B8",
             color: "white",
-            paddingTop: "8px",
           }}
         >
           Write
-          <div style={{ fontSize: "8px" }}>
-            <SmartButton size="20" style={{ marginTop: "-5px" }} />
-          </div>
+          <button
+            className="keypad-info"
+            name="write"
+            onClick={onClick}
+            style={{
+              backgroundColor: "#3282B8",
+              color: "white",
+            }}
+          >
+            QuickNum
+          </button>
         </button>
         <button className="keypad" name="1" onClick={onClick}>
           1
@@ -258,9 +322,14 @@ function App() {
           style={{ backgroundColor: "#285430", color: "white" }}
         >
           ANS
-          <div style={{ fontSize: "8px" }}>
+          <button
+            className="keypad-info"
+            name="ans"
+            onClick={onClick}
+            style={{ backgroundColor: "#285430", color: "white" }}
+          >
             {ans ? (ans.length > 9 ? `${ans.slice(0, 8)}…` : ans) : "직전답"}
-          </div>
+          </button>
         </button>
         <button className="keypad" name="0" onClick={onClick}>
           0
@@ -277,31 +346,36 @@ function App() {
       </div>
       <div className="logo">calculog</div>
 
-      <style jsx>{`
-        .App {
-          position: relative;
-          width: 360px;
-          height: ${document.documentElement.clientHeight}px;
-          border-radius: 10px;
-          background-color: #222;
-          color: white;
-          text-align: center;
-        }
+      <style jsx>
+        {`
+          .App {
+            position: relative;
+            width: 360px;
+            height: ${document.documentElement.clientHeight}px;
+            border-radius: 10px;
+            background-color: #222;
+            color: white;
+            text-align: center;
+          }
 
-        .log {
-          position: relative;
-          height: ${document.documentElement.clientHeight - 450}px;
-          margin: 2px;
-          border-radius: 5px;
-          padding: 5px 10px;
-          background-color: #fff5e4;
-          color: black;
-          text-align: left;
-          font-size: 18px;
-          word-break: break-all;
-          overflow-y: auto;
-        }
-      `}</style>
+          .log-box {
+            position: relative;
+            height: ${document.documentElement.clientHeight - 450}px;
+            margin: 2px;
+            border-radius: 5px;
+            padding: 5px 0 5px 10px;
+            background-color: #fff5e4;
+            color: black;
+            text-align: left;
+            font-size: 18px;
+            word-break: break-all;
+          }
+          .log {
+            height: ${document.documentElement.clientHeight - 460}px;
+            overflow-y: auto;
+          }
+        `}
+      </style>
     </div>
   )
 }
